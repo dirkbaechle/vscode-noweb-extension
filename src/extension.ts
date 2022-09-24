c603da15c964e4c0bbbe4174e158a8a89050d029
Dirk Baechle <dl9obn@darc.de>
2022-09-24 18:56:30 +0200

    Add first basic tests

--- src/extension.ts
+++ src/extension.ts
@@ -36,7 +36,7 @@ const legend = (function () {
     return new vscode.SemanticTokensLegend(types, modifiers);
 })();
 
-interface IToken {
+export interface IToken {
     line: number;
     start: number;
     length: number;
@@ -48,22 +48,22 @@ interface IToken {
 // Parsing mode
 enum Mode { tex = 1, code };
 // Token types/modifiers for our detected items
-const chunkStartType = 'function';
-const chunkStartModifiers = ['declaration'];
-const definitionType = 'variable';
-const referenceType = 'keyword';
-const undefinedReferenceType = 'comment';
-const codeType = 'string';
+export const chunkStartType = 'function';
+export const chunkStartModifiers = ['declaration'];
+export const definitionType = 'variable';
+export const referenceType = 'keyword';
+export const undefinedReferenceType = 'comment';
+export const codeType = 'string';
 
 // Regexes
 const reDefinition = /^<<(.*)>>=\s*$/;
 const reReference = /<<(.*?)>>/g;
 const reChunkStart = /^@\s*.*$/;
 
-class NowebTokenProvider implements vscode.DocumentSemanticTokensProvider {
+export class NowebTokenProvider implements vscode.DocumentSemanticTokensProvider {
     provideDocumentSemanticTokens(doc: vscode.TextDocument, cancel: vscode.CancellationToken) {
         const builder = new vscode.SemanticTokensBuilder();
-        this._parse(doc.getText(), cancel).forEach((token) => builder.push(
+        this.parse(doc.getText(), cancel).forEach((token) => builder.push(
             token.line, token.start, token.length,
             this._encodeType(token.type),
             this._encodeModifiers(token.modifiers),
@@ -71,7 +71,7 @@ class NowebTokenProvider implements vscode.DocumentSemanticTokensProvider {
         return builder.build();
     }
 
-    private _parse(text: string, cancel: vscode.CancellationToken): IToken[] {
+    public parse(text: string, cancel: vscode.CancellationToken): IToken[] {
         let tokens: IToken[] = [];
         let keywords = {defines: new Set<string>()};
         let mode = Mode.tex;
@@ -206,14 +206,16 @@ class NowebTokenProvider implements vscode.DocumentSemanticTokensProvider {
     }
 };
 
-class NowebFoldingRangeProvider implements vscode.FoldingRangeProvider {
+export class NowebFoldingRangeProvider implements vscode.FoldingRangeProvider {
     provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.FoldingRange[] {
-        let ranges: vscode.FoldingRange[] = [];
-        const docText = document.getText();
+        return this.parse(document.getText(), token);
+    }
 
+    public parse(text: string, token: vscode.CancellationToken): vscode.FoldingRange[] {
+        let ranges: vscode.FoldingRange[] = [];
         let lastFoldline = 0;
         let i = 0;
-        const lines = docText.split(/\r?\n/);
+        const lines = text.split(/\r?\n/);
         while (i < lines.length) {
             const line = lines[i];
 
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,31 @@
-import * as assert from 'assert';
-
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
+import * as assert from 'assert';
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
+suite('Noweb semantic highlighting', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Basic tests', function () {
+		// Empty file
+		let tokens = buildSemanticTokens("");
+		assert.strictEqual(tokens.length, 0);
+		// Single chunk start
+		tokens = buildSemanticTokens("@ ");
+		assert.strictEqual(tokens.length, 1);
+		assert.deepStrictEqual(tokens[0], t(0, 0, 1, myExtension.chunkStartType, myExtension.chunkStartModifiers));
 	});
 });
+
+function buildSemanticTokens(content: string): myExtension.IToken[] {
+	let cancel = new vscode.CancellationTokenSource();
+	const semProv = new myExtension.NowebTokenProvider();
+	return semProv.parse(content, cancel.token);
+}
+
+function t(line: number, start: number, length: number,
+	       type: string, modifiers: string[] = []): myExtension.IToken {
+	const keyword = "";
+	return { line, start, length, type, modifiers, keyword };
+}
